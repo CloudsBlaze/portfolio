39fa3be8598c9267f0abddb5540c568b86dcef36
mansoor_rehman <mansoorurrehman793@gmail.comm>
2023-10-24 13:37:49 +0500

    add serializersand, models,urls,views admin

--- website/settings.py
+++ website/settings.py
@@ -36,7 +36,10 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
-    "django.contrib.staticfiles","contact_us",
+    "django.contrib.staticfiles",
+    'rest_framework',
+    "contact_us"
+
 ]
 
 MIDDLEWARE = [
