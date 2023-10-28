1ce093d68e56322a1c7e10ad8a4476477841ae6e
mansoor_rehman <mansoorurrehman793@gmail.comm>
2023-10-28 06:36:38 +0500

    update admin

--- events/admin.py
+++ events/admin.py
@@ -1,8 +1,21 @@
 from django.contrib import admin
 from .models import Event,Participant
 
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('title', 'start_date', 'end_date', 'status_type')
+    list_filter = ('status_type', 'create_date')
+    search_fields = ('title','organizer')
+
+class ParticipantAdmin(admin.ModelAdmin):
+    list_display = ('user', 'event')
+    list_filter = ('event__title', 'user__username')
+    search_fields = ('event__title', 'user__username')
+    
+
+
 # Register your models here.
-admin.site.register(Event)
-admin.site.register(Participant)
+admin.site.register(Event, EventAdmin)
+admin.site.register(Participant, ParticipantAdmin)
 
 
