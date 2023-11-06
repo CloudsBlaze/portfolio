50f94c5cf3a0902e432a8e5d776f6ff8656936f8
muhammadumarjaved44 <muhammadumarjaved44@gmail.com>
2023-11-06 21:17:52 +0500

    add the template for contact us

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from frontend.views import (
-    # ContactUsView,
+    ContactUsIndex,
     HomeIndex,
 #     ServicesIndex,
 #    AboutIndex,
@@ -11,7 +11,7 @@ urlpatterns = [
     path("", HomeIndex.as_view()),
     # path("services", ServicesIndex.as_view()),
     # path("about-us", AboutIndex.as_view()),
-    # path("contact-us", ContactUsView.as_view()),
+    path("contact-us", ContactUsIndex.as_view()),
     # path("api", APIIndex.as_view()),
     # path("contact-us/<str:pk>", ContactUsView.as_view()),
 ]
