b4332db47b66fc0416311830c380512d1cb1c26e
muhammadumarjaved44 <muhammadumarjaved44@gmail.com>
2023-11-08 01:21:37 +0500

    contact us form submission

--- frontend/urls.py
+++ frontend/urls.py
@@ -4,14 +4,16 @@ from frontend.views import (
     ContactUsIndex,
     ServiceIndex,
     AboutIndex,
+    thank_you
 #    APIIndex
 )
 
 urlpatterns = [
-    path("", HomeIndex.as_view()),
-    path("contact-us", ContactUsIndex.as_view()),
+    path("", HomeIndex.as_view(), name="home"),
+    path("contact-us", ContactUsIndex.as_view(), name="contact-submit"),
     path("services", ServiceIndex.as_view()),
     path("about-us", AboutIndex.as_view()),
+    path('thank-you/', thank_you, name='thank-you-page')
     # path("api", APIIndex.as_view()),
     # path("contact-us/<str:pk>", ContactUsView.as_view()),
 ]
