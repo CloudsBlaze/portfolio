3897bc4773cf794f63fc73d166f7dc4fb140bf7d
muhammadumarjaved44 <muhammadumarjaved44@gmail.com>
2023-11-07 23:31:40 +0500

    adding templates pages

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,17 +1,17 @@
 from django.urls import path
 from frontend.views import (
-    ContactUsIndex,
     HomeIndex,
-#     ServicesIndex,
-#    AboutIndex,
+    ContactUsIndex,
+    ServiceIndex,
+    AboutIndex,
 #    APIIndex
 )
 
 urlpatterns = [
     path("", HomeIndex.as_view()),
-    # path("services", ServicesIndex.as_view()),
-    # path("about-us", AboutIndex.as_view()),
     path("contact-us", ContactUsIndex.as_view()),
+    path("services", ServiceIndex.as_view()),
+    path("about-us", AboutIndex.as_view()),
     # path("api", APIIndex.as_view()),
     # path("contact-us/<str:pk>", ContactUsView.as_view()),
 ]
