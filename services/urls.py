75075ccf3bf07619268906aac8274601c2e57592
mansoor_rehman <mansoorurrehman793@gmail.comm>
2023-11-14 21:10:26 +0500

    changing in urls.py and views.py in services app

--- services/urls.py
+++ services/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ServiceAPI,ServiceCategoryAPI
+from .views import ServiceAPI,ServiceCategoryAPI,ServiceImageAPI
 
 
 
@@ -11,5 +11,7 @@ urlpatterns =[
     path("services/", ServiceAPI.as_view()),
     path("services/<int:pk>/", ServiceAPI.as_view()),
     path("servicecategory/", ServiceCategoryAPI.as_view()),
-    path("servicecategory/<int:pk>/", ServiceCategoryAPI.as_view())
+    path("servicecategory/<int:pk>/", ServiceCategoryAPI.as_view()),
+    path("serviceimage/", ServiceImageAPI.as_view()),
+    path("serviceimage/<int:pk>/", ServiceImageAPI.as_view())
 ]
