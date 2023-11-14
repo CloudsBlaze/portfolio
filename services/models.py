e4d0a302d718a52168ab7d16c1e5461c1982fda8
mansoor_rehman <mansoorurrehman793@gmail.comm>
2023-11-14 21:04:48 +0500

    create model in services app and changing in Service model

--- services/models.py
+++ services/models.py
@@ -1,11 +1,10 @@
 from django.db import models
+from image.models import Images
 
 # Create your models here.
 class Service(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
-    image1 = models.ImageField(upload_to='service_images/', blank=True, null=True)
-    image2 = models.ImageField(upload_to='service_images/', blank=True, null=True)
     service_category = models.ForeignKey('ServiceCategory', on_delete=models.CASCADE)
     create_date = models.DateTimeField(auto_now_add=True)
     update_date = models.DateTimeField(auto_now=True)
@@ -21,3 +20,8 @@ class ServiceCategory(models.Model):
 
     def __str__(self):
         return self.name
+    
+class ServiceImage(Images):
+    service_images = models.ForeignKey(
+        Service, related_name="service_images", on_delete=models.CASCADE
+    )
