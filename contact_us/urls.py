143ed783c456b9722e23b39033914a54c4dfe5cd
muhammadumarjaved44 <muhammadumarjaved44@gmail.com>
2023-12-23 15:08:44 +0500

    layout issue fix

--- contact_us/urls.py
+++ contact_us/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from contact_us.views import ContactUsAPI
 
 
-
-urlpatterns =[
-    path("contact-us/", ContactUsAPI.as_view(), name="contact-us"),
-    path("contact-us/<int:pk>/", ContactUsAPI.as_view(), name="contact-us")
+urlpatterns = [
+    path("contact-us/", ContactUsAPI.as_view(), name="api-contact-us"),
+    path("contact-us/<int:pk>/", ContactUsAPI.as_view(), name="api-contact-us"),
 ]
